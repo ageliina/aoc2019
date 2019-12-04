8ea71db8deb3b62005ed37a86df784f3c20832fa
Akke Viitanen <akke.viitanen@helsinki.fi>
2019-12-04 07:32:11 +0200

    Solved day 04

---
+++ 04/04.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Author: Akke Viitanen
+# Email: akke.viitanen@helsinki.fi
+
+"""
+A python3 script template. Remove this line and add your description.
+"""
+
+low, high = 284639, 748759
+
+count = 0
+for i in range(low, high + 1):
+    number = str(i)
+    if sorted(number) != list(number):
+        continue
+    g = {j: number.count(str(j)) for j in range(10)}
+    if 2 not in g.values():
+        continue
+    count += 1
+print(count)
